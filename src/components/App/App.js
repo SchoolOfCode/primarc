af1d0f230393cc3270e2c386a1fb00ce70456b84
jafcoder <joefarr113@gmail.com>
2022-06-10 13:58:58 +0100

    responsive navbar and dark mode added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
+import Navbar from "../navbar";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function onClick() {
+    setDarkMode(!darkMode);
+    console.log("dark mode enabled");
+  }
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [darkMode]);
   return (
     <div className="App">
       <header className="App-header">
-        <p>Primarc</p>
+        <Navbar onClick={onClick} />
       </header>
     </div>
   );
