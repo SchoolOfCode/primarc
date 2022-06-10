d7629f3fe0e669b19812f34fd97091a3fb1eaa24
AmandaRichards <Richards.amanda92@gmail.com>
2022-06-10 13:14:51 +0100

    data renders

--- src/components/CategoryContainer/index.js
+++ src/components/CategoryContainer/index.js
@@ -1,20 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Card from '../card'
 
 function CategoryContainer({category}) {
+
+   const [items, setItems] = useState(null)
+      
 useEffect(()=>{
     async function fetchData (){
     const response = await fetch(`https://fakestoreapi.com/products/category/${category.toLowerCase()}`)
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-    } fetchData()
-}, [])
+    const data = await response.json()
+            // // .then(res=>res.json())
+            // .then(json=>console.log(json))
+            setItems(data)
+    } 
+    fetchData()
+ 
+    console.log(items)
+}, [items])
 
 
   return (
     <div>
     <h2>{category}</h2>
+   {items? <Card data={items[0]}/> : `no results`}
     </div>
-  )
+  )  
 }
 
 export default CategoryContainer
